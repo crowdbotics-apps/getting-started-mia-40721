89f504883422dd7bce76a6618a705321d98240e5
Mia Manic <mia.manic@crowdbotics.com>
2023-05-22 19:18:21 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,235 @@
+import { ImageBackground } from "react-native";
+import { Text } from "react-native";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><View style={styles.EETHqNzV}><Text style={styles.UZhMCeLW}>{"Yuga Wheel"}</Text><ImageBackground style={styles.ECFUhEXc} source={require("./back 1.png")} resizeMode="cover"></ImageBackground></View><View style={styles.aRfhSHYS}><View style={styles.tcZeuqZP}><ImageBackground style={styles.IOGUgKdP} source={require("./michael-dam-mEZ3PoFGs_k-unsplash.jpg")} resizeMode="cover"></ImageBackground></View><Text style={styles.TXwWBpTd}>{"Breakfast"}</Text></View></ScrollView>
+    <View style={styles.SvaKGdqD}></View><View style={styles.SqGlNCYy}></View><View style={styles.mQJdgfAL}></View><View style={styles.DNfiigLI}></View><View style={styles.qMmclCAg}></View><View style={styles.IqKNGUSR}></View><View style={styles.eUtPyign}></View><Text style={styles.dDYIKmeD}>Morning Yoga</Text><Text style={styles.hpetRGxp}>{"Spiritual work"}</Text><Text style={styles.Fvogfrwf}>{"Self Care"}</Text><Text style={styles.cWXOqWvU}>{"Education"}</Text><Text style={styles.oqcNkcuL}>{"Dinner"}</Text><Text style={styles.WqKgjiJg}>{"Sleep"}</Text></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  EETHqNzV: {
+    height: 78,
+    width: 360,
+    backgroundColor: "#123051",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 0,
+    left: 0
+  },
+  UZhMCeLW: {
+    width: 100,
+    height: 20,
+    lineHeight: 14,
+    fontSize: 16,
+    borderRadius: 0,
+    position: "absolute",
+    left: 130,
+    color: "#ffffff",
+    top: 22,
+    textAlign: "center"
+  },
+  aRfhSHYS: {
+    height: 506,
+    width: 360,
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    color: "#777777",
+    position: "absolute",
+    top: 62,
+    left: 0,
+    borderColor: "#000000"
+  },
+  ECFUhEXc: {
+    width: 20,
+    height: 20,
+    position: "absolute",
+    left: 18,
+    top: 20
+  },
+  tcZeuqZP: {
+    height: 200,
+    width: 200,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 153,
+    left: 80
+  },
+  IOGUgKdP: {
+    width: 120,
+    height: 120,
+    position: "absolute",
+    top: 40,
+    left: 40,
+    transform: [{
+      rotate: "0deg"
+    }]
+  },
+  SvaKGdqD: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#F32A00",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 192,
+    left: 155,
+    borderColor: "#F79781",
+    borderWidth: 2
+  },
+  SqGlNCYy: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#86BB00",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 203,
+    left: 217,
+    borderColor: "#C4DD81",
+    borderWidth: 2
+  },
+  mQJdgfAL: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#99439D",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 262,
+    left: 255,
+    borderColor: "#CCA3CF",
+    borderWidth: 2
+  },
+  DNfiigLI: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#77CCCD",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 337,
+    left: 250,
+    borderColor: "#BBE6E7",
+    borderWidth: 2
+  },
+  qMmclCAg: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#E25297",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 392,
+    left: 201,
+    borderColor: "#E9ADCA",
+    borderWidth: 2
+  },
+  IqKNGUSR: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#86BB00",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 390,
+    left: 125,
+    borderColor: "#C4DD81",
+    borderWidth: 2
+  },
+  eUtPyign: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#AAABAD",
+    borderRadius: 90,
+    color: "#777777",
+    position: "absolute",
+    top: 263,
+    left: 56,
+    borderColor: "#D6D7D8",
+    borderWidth: 2
+  },
+  TXwWBpTd: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 118,
+    left: 236
+  },
+  dDYIKmeD: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 165,
+    left: 121
+  },
+  hpetRGxp: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 238,
+    left: 276
+  },
+  Fvogfrwf: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 325,
+    left: 285
+  },
+  cWXOqWvU: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 439,
+    left: 228
+  },
+  oqcNkcuL: {
+    width: 50,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 435,
+    left: 113
+  },
+  WqKgjiJg: {
+    width: 38,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    position: "absolute",
+    top: 235,
+    left: 37
+  }
+});
+export default Untitled1;
